216702b59ea9804e87727219f579afbc6a47d8a0
Orion <serg.batumi2022@gmail.com>
2023-11-17 21:52:02 +0400

    feat: complete getSumOfNumbers task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -598,8 +598,11 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  if (!Number.isInteger(x1) || !Number.isInteger(x2) || !Number.isInteger(x3)) {
+    return +(x1 + x2 + x3).toFixed(2);
+  }
+  return x1 + x2 + x3;
 }
 
 /**
